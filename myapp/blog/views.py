6e210e1291df1776dc323bb3ddd37f84f7e10702
Gokul <gokulchandan24@gmail.com>
2025-06-04 19:45:32 +0530

    component updated

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index (request):
       return HttpResponse("Hello world")
+
+def detail (request):
+      return HttpResponse("you are viewing post details")
